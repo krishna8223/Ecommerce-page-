505c4beff6bbb42ab4dd560dd9dc158ea58070c2
krishna8223 <krishnaverma.kv822@gmail.com>
2022-04-27 12:43:01 +0530

    added rel

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className=' h-48 w-full flex items-center bg-orange-600'>
         <div className='icons flex justify-center gap-8 w-[80%] mx-auto '>
-            <a target='_blank' href="https://github.com/krishna8223/Ecommerce-page-" className='flex gap-8 text-5xl ' >
+            <a target='_blank' rel="noreferrer" href="https://github.com/krishna8223/Ecommerce-page-" className='flex gap-8 text-5xl ' >
              View code on  <AiFillGithub className='hover:text-white ' /> 
             </a>
             
