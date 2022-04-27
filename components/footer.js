04660c95d30b6b8e95f33a53550a4c38be26dc52
krishna8223 <krishnaverma.kv822@gmail.com>
2022-04-27 14:43:44 +0530

    footer height change

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ import { GrInstagram } from 'react-icons/gr'
 
 function Footer() {
   return (
-    <div className=' h-48 w-full flex items-center bg-orange-600'>
+    <div className=' h-36 w-full flex items-center bg-orange-600'>
         <div className='icons flex justify-center gap-8 w-[80%] mx-auto '>
             <a target='_blank' rel="noreferrer" href="https://github.com/krishna8223/Ecommerce-page-" className='flex gap-8 text-5xl ' >
              View code on  <AiFillGithub className='hover:text-white ' /> 
